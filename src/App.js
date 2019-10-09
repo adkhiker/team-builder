fc80cb5498780f38e7d195988687aa507243e5ef
adkhiker <poirier.greg@gmail.com>
2019-10-09 17:09:02 -0400

    Added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handleChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(name)}
+      <form>
+        <label>
+          Username:
+          <input type="text" onChange={event => handleChange(event)} />
+        </label>
+        <button onSubmit={() => handleSubmit()}>Submit!</button>
+      </form>
     </div>
   );
 }
